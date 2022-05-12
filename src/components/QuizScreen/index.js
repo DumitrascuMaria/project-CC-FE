af33e9041e2a1640464faf15cb3dacb6594362fc
DumitrascuMaria <dumitrascusandra18@stud.ase.ro>
2022-05-12 15:35:58 +0300

    finished design for quizScreen

--- src/components/QuizScreen/index.js
+++ src/components/QuizScreen/index.js
@@ -4,6 +4,8 @@ import "./index.scss";
 
 const QuizScreen = ({ studentName }) => {
   const [questions, setQuestions] = useState([]);
+  const [grade, setGrade] = useState(0);
+  const [isFinished, setIsFinished] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios.get(
@@ -20,27 +22,67 @@ const QuizScreen = ({ studentName }) => {
     fetchData();
   }, []);
 
-  console.log(questions);
+  const selectAnswerHandler = () => {
+    const newGrade = grade + 1;
+    setGrade(newGrade);
+  };
+
+  const finishQuizHandler = async () => {
+    setIsFinished(true);
+    try {
+      let response = await axios.post(
+        `${process.env.REACT_APP_API_URL}/sendMail`,
+        {
+          receiver: "dumitrascusandra18@stud.ase.ro",
+          sender: "dumitrascusandra18@stud.ase.ro",
+          subject: "test",
+          msg: "test",
+        }
+      );
+
+      if (response.status === 200) {
+        alert("Your teacher will receive your grade.");
+      }
+    } catch (error) {
+      alert("Something went wrong");
+      console.log(error);
+    }
+  };
+
   return (
     <div className="quiz-screen">
       <p>Student: {studentName} </p>
       <h1>Test</h1>
-      <div className="questions-container">
-        {questions &&
-          questions.map((question, index) => (
-            <div className="question" key={index}>
-              <p className="question-text">
-                <span>{index + 1}. </span>
-                {question.questionText}
-              </p>
-              <div className="answers-container">
-                <div className="answer">{question.questionRightAnswer}</div>
-                <div className="answer">{question.questionWrongAnswer}</div>
-              </div>
-            </div>
-          ))}
-      </div>
-      <div className="finish-button">FINISH</div>
+      {!isFinished ? (
+        <>
+          <div className="questions-container">
+            {questions &&
+              questions.map((question, index) => (
+                <div className="question" key={index}>
+                  <p className="question-text">
+                    <span>{index + 1}. </span>
+                    {question.questionText}
+                  </p>
+                  <div className="answers-container">
+                    <button className="answer" onClick={selectAnswerHandler}>
+                      {question.questionRightAnswer}
+                    </button>
+                    <button className="answer">
+                      {question.questionWrongAnswer}
+                    </button>
+                  </div>
+                </div>
+              ))}
+          </div>
+          <div className="finish-button" onClick={finishQuizHandler}>
+            FINISH
+          </div>
+        </>
+      ) : (
+        <h2>
+          Your score is: {grade} / {questions.length}
+        </h2>
+      )}
     </div>
   );
 };
